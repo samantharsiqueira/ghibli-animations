0f0bd3e2ceb4fdb45537ad599c1a1fe3caeb5771
samantha <samantharsiqueira@gmail.com>
2024-03-21 13:15:11 +0000

    last one

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     if (isFavourite) {
       setFavorites(favorites.filter((favFilm) => favFilm.id !== film.id));
     } else {
-      setFavorites([...favorites, film]);
+      setFavorites([...favorites, film]);q
     }
   };
   return (
